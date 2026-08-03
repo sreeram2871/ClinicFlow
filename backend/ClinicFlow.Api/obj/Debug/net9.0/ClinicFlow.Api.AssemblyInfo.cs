25a1d988177896d8d6f5708dff7150042aa17814
Sreeram Kommarolu <komarolusreeram123@gmail.com>
2026-08-03 15:48:41 +0530

    Code quality pass: removed mismatched MediatR extension package, zero build warnings now

--- backend/ClinicFlow.Api/obj/Debug/net9.0/ClinicFlow.Api.AssemblyInfo.cs
+++ backend/ClinicFlow.Api/obj/Debug/net9.0/ClinicFlow.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+026ec88366c66be2815d25afc4dd98ece916cbf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2928ad9fc172869f3dc7f398d669eda8a49b21d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
